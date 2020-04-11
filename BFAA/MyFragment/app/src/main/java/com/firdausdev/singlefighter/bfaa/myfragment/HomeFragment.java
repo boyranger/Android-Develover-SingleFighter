c157f9915cd4a8c834e4389cc2a42a9e980ba450
Fauzi AR-AbaAdiba <fauziboyranger@gmail.com>
2020-04-11 11:50:17 +0700

    fragment
    codeLabs Fragment

---
+++ BFAA/MyFragment/app/src/main/java/com/firdausdev/singlefighter/bfaa/myfragment/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.firdausdev.singlefighter.bfaa.myfragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this
+        //https://www.bignerdranch.com/blog/understanding-androids-layoutinflater-inflate/
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnCategory = view.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
